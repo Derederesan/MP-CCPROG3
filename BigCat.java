7f9d52ad9cd8c3733a9bd2dc704ade8967364fc8
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-07-10 17:12:10 +0800

    Update BigCat.java

--- BigCat.java
+++ BigCat.java
@@ -1,6 +1,15 @@
 public class BigCat extends Animal
 {
     /**
+    *Constructs a new BigCat that takes the input and assigns it to its attributes 
+    */
+    public BigCat(String name, int rank, Space position)
+    {
+        this.name = name; 
+        this.rank = rank; 
+        this.position = position; 
+    }
+    /**
      * Checks if a bigcat can jump from land to river 
      * @param position the space that the bigcat intends to jump to 
      * @return true if there is no mouse in the river 
