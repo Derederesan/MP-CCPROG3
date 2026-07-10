49e7a2ce3a7ee750ae5f2dc51899c9c347d685dc
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-07-10 21:29:34 +0800

    Update Tiger.java

--- Tiger.java
+++ Tiger.java
@@ -1,5 +1,13 @@
+/**
+    * The subclass of the abstract superclass BigCat 
+    */
+    
 public class Tiger extends BigCat
 {
+    /**
+    * Constructs the class Tiger and 
+    */
+    
     public Tiger(Space position)
     {
         super("Tiger", 6, position);
