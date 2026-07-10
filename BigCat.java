1ff93904379cb82425ff132c8a67af228a3fbf28
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-07-10 17:10:08 +0800

    Update BigCat.java

--- BigCat.java
+++ BigCat.java
@@ -2,13 +2,13 @@ public class BigCat extends Animal
 {
     /**
      * Checks if a bigcat can jump from land to river 
-     * @param target the space that the bigcat intends to jump to 
+     * @param position the space that the bigcat intends to jump to 
      * @return true if there is no mouse in the river 
      */
-    public boolean jumpRiver(Space target)
+    public boolean canMove(Space position)
     {
         //check if there is no mouse along the way 
-        if(target.getAnimal().getRank() != 1 )
+        if(position.getAnimal().getRank() != 1 )
         {
             return true; 
         }
