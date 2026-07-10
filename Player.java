8c9f224c054800c1a2944dca34adeb2237973162
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-07-10 21:33:45 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,7 @@
+/**
+    *  Constructs a new Space that takes the input provided 
+    * and assigns it to the specified attributes of the class.
+*/
 import java.util.ArrayList;
 
 public class Player 
@@ -7,27 +11,51 @@ public class Player
     private char move; 
     private ArrayList<Animal> animals; 
 
+    /**
+    *  Constructs a new Player that takes the input provided 
+    * and assigns it to the specified attributes of the class.
+    */
     public Player(int playerNum)
     {
         this.playerNum = playerNum;
         this.animals = new ArrayList<Animal>();
     }
 
+    /**
+    * Sets the color of the player's animal 
+    * @param color the chosen color 
+    */
+    
     public void setColor(String color)
     {
         this.color = color; 
     }
 
+    /**
+    * Takes the current movement the player intends to execute 
+    * @param movement the movement to be set 
+    */
+    
     public void setMove(char movement)
     {
         this.movement = movement; 
     }
 
+    /**
+    * Gets the player's number 
+    * @return the player's number 
+    */
+    
     public int getPlayerNum()
     {
         return this.playerNum; 
     }
 
+    /**
+    *  Gets the ArrayList of the player's animals 
+    * @return the ArrayList of the player's animals 
+    */
+    
     public ArrayList<Animal> getAnimals()
     {
         return this.animals; 
