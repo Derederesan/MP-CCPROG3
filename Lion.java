5ac20fb600d05e2d7aafb3be8984c3a7b4cdddbb
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-07-11 07:08:29 +0800

    Update Lion.java

--- Lion.java
+++ Lion.java
@@ -1,8 +1,12 @@
 /**
-* A lion 
+* A lion is the subclass of the abstract superclass BigCat 
 */
 public class Lion extends BigCat
 {
+    /**
+    *  Constructs a new Space that takes the input provided 
+    * and assigns it to the specified attributes of the class.
+    */
     public Lion(Space position)
     {
         super("Lion", 7, position);
