bbd37d20e996d64d0b980f368dc17c26b444ebd2
akinicoledev <aki_cartago@dlsu.edu.ph>
2026-07-11 17:53:44 +0800

    Update Mouse.java

--- Mouse.java
+++ Mouse.java
@@ -1,31 +1,31 @@
 /**
-    *  A Mouse is a subclass of the superclass animal. It checks wheter a mouse can enter a  river, 
-    * and whether or not it can capture a specific target. 
+    *  A Mouse is a subclass of the superclass animal. It checks wheter a mouse can enter a  river,
+    * and whether or not it can capture a specific target.
     */
 
 public class Mouse extends Animal
 {
    /**
-   * Constructs a new Mouse that takes the provided input 
-   * and assigns it to the attributes of the class 
+   * Constructs a new Mouse that takes the provided input
+   * and assigns it to the attributes of the class
    */
-    public Mouse(Space position)
-    {
-        super("Mouse", 1, position);
-    }
+   public Mouse(Space position)
+   {
+       super("Mouse", 1, position, 0, 0, 0);
+   }
 
    /**
-   * Checks if Mouse can move to a specific position 
-   * @param position the space where mouse intends to move 
+   * Checks if Mouse can move to a specific position
+   * @param position the space where mouse intends to move
    */
     public boolean canMove(Space position)
     {
-       //if river can cross 
+       //if river can cross
        if (position.isRiver())
        {
-          return true; 
+          return true;
        }
-       //if not river, check if it can cross 
+       //if not river, check if it can cross
         return super.canMove(position);
     }
 
@@ -36,14 +36,14 @@ public class Mouse extends Animal
         {
             return false;
         }
-        
+
         // A mouse on land may not capture a mouse on the river[cite: 1]
         if (this.getCurrentSpace().isLand() && target.getCurrentSpace().isRiver())
         {
             return false;
         }
 
-      
+
 
         // Mouse can capture an elephant (rank 8) or another mouse (rank 1)[cite: 1]
         return target.getRank() == 8 || target.getRank() == 1;
