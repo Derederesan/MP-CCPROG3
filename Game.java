32e7faab29a283cf8d59fe2c1c4d97290ddfe9df
akinicoledev <aki_cartago@dlsu.edu.ph>
2026-07-11 17:52:56 +0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -3,114 +3,106 @@
  * serves as the checker on which players should go first, and checks if a player has won. 
  */
 
-public class Game 
-{
-    private int currentTurn = -1; 
-    private Player player1; 
-    private Player player2; 
-    private Board board; 
-    private boolean isGameOver; 
-    private Player winner; 
+public class Game {
+    private int currentTurn = -1;
+    private Player player1;
+    private Player player2;
+    private Board board;
+    private boolean isGameOver;
+    private Player winner;
 
     /**
-     * Constructs a new Game that takes the input provided and assigns it 
-     * to the specified attributes of the class. 
+     * Constructs a new Game that takes the input provided and assigns it
+     * to the specified attributes of the class.
      */
-    public Game(Player player1, Player player2)
-    {
-        this.player1 = player1; 
+    public Game(Player player1, Player player2) {
+        this.player1 = player1;
         this.player2 = player2;
-        this.board = new Board(); 
-        this.isGameOver = false; 
-        this.winner = null; 
+        this.board = new Board();
+        this.isGameOver = false;
+        this.winner = null;
     }
-    /**
-     * Compares the chosen animal piece of each player 
-     * and uses it to determine which player goes first. 
-     * @param p1Choice the chosen animal piece of player 1 
-     * @param p2Choice the chosen animal piece of player 2
-     * @return the player that goes first 
-     */
-    public int firstPick(Animal p1Choice, Animal p2Choice)
-    {
-      if(p1Choice.getRank()>p2Choice.getRank())
-      {
-        System.out.println("Player 1 moves first.");
-        this.currentTurn=1; 
-        return 1; 
-      }  
-      else if(p1Choice.getRank()<p2Choice.getRank())
-      {
-        System.out.println("Player 2 moves first."); 
-        this.currentTurn=2; 
-        return 2;
-      }
-      else
-      {
-        System.out.println("Tie! Players must pick again.");
-        return -1; 
-      }
-    } 
 
     /**
-     * Keeps track of which player is currently playing 
+     * Compares the chosen animal piece of each player
+     * and uses it to determine which player goes first.
+     *
+     * @param p1Choice the chosen animal piece of player 1
+     * @param p2Choice the chosen animal piece of player 2
+     * @return the player that goes first
      */
-    public void updateTurn ()
-    {
-        if(this.currentTurn==1)
-        {
-            this.currentTurn=2;
-        }
-        else if(this.currentTurn==2)
-        {
-            this.currentTurn=1; 
+    public int firstPick(Animal p1Choice, Animal p2Choice) {
+        if (p1Choice.getRank() > p2Choice.getRank()) {
+            System.out.println("Player 1 moves first.");
+            this.currentTurn = 1;
+            return 1;
+        } else if (p1Choice.getRank() < p2Choice.getRank()) {
+            System.out.println("Player 2 moves first.");
+            this.currentTurn = 2;
+            return 2;
+        } else {
+            System.out.println("Tie! Players must pick again.");
+            return -1;
         }
     }
+
+    /**
+     * Keeps track of which player is currently playing
+     */
+    public void updateTurn() {
+        if (this.currentTurn == 1) {
+            this.currentTurn = 2;
+        } else if (this.currentTurn == 2) {
+            this.currentTurn = 1;
+        }
+    }
+
     /*
-    *checks if a player has won 
-    */
+     *checks if a player has won
+     */
     public void checkWin()
     {
-        //checks who current player is 
-        Player currentPlayer; 
-        if(this.currentTurn ==1)
+        Player currentPlayer;
+
+        if (this.currentTurn == 1)
         {
-            currentPlayer=this.player1; 
+            currentPlayer = this.player1;
         }
         else
         {
-            currentPlayer=this.player2;
+            currentPlayer = this.player2;
         }
-        //iterates through player's animals to check their positions 
-        for(Animal animal :currentPlayer.getAnimals())
-        {
-            Space currentSpace =animal.getCurrentSpace(); 
 
-            //check if the space isEnemyDen
-            if(currentSpace.isEnemyDen(this.currentTurn))
+        for (Animal animal : currentPlayer.getAnimals())
+        {
+            Space currentSpace = animal.getCurrentSpace();
+
+            if (currentSpace.isEnemyDen(this.currentTurn))
             {
-                this.winner = currentPlayer; 
-                this.isGameOver = true; 
-                Syatem.out.println("Player " + this.currentTurn + "has won!"); 
+                this.winner = currentPlayer;
+                this.isGameOver = true;
+                System.out.println("Player " + this.currentTurn + " has won!");
+                return;
             }
         }
-        //update isGameover to true
-       
-    }
-    /**
-    *Returns the current player 
-    *@return the current turn of the player 
-    */
-  public int getCurrentTurn()
-    {
-        return this.currentTurn; 
     }
 
     /**
-    *Returns the winner 
-    *@return the winner 
-    */
-    public Player getWinner()
-    {
-        return this.winner; 
+     * Returns the current player
+     *
+     * @return the current turn of the player
+     */
+    public int getCurrentTurn() {
+        return this.currentTurn;
     }
+
+    /**
+     * Returns the winner
+     *
+     * @return the winner
+     */
+    public Player getWinner() {
+        return this.winner;
+    }
+
+}
