335fee6dc63571bad3c2aca51be27ab37b4e1b8a
akinicoledev <aki_cartago@dlsu.edu.ph>
2026-07-11 17:55:21 +0800

    Update Tiger.java

--- Tiger.java
+++ Tiger.java
@@ -8,9 +8,9 @@ public class Tiger extends BigCat
     * Constructs the a new Tiger that takes the provided input and assigns it 
     * to the specified attributes of the class 
     */
-    
+
     public Tiger(Space position)
     {
-        super("Tiger", 6, position);
+        super("Tiger", 6, position, 0, 0, 0);
     }
 }
