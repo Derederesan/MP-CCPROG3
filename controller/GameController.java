a3d9e94b8f331f8a186bf11a6d9240715d42f44a
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-07-22 15:53:20 +0800

    Update GameController.java

--- controller/GameController.java
+++ controller/GameController.java
@@ -1,12 +1,56 @@
+import java.awt.event. *;
+import javax.swing.*;
+
 package controller;
 
-public class GameController {
-  private Game model; 
-  private GameView view; 
+public class GameController implements KeyListener{
+    private Game model; 
+    private GameView view; 
 
-  public GameController(Game model, GameView view)
-  {
-    this.model = model; 
-    this.view = view; 
-  }
+    public GameController(Game model, GameView view)
+    {
+      this.model = model; 
+      this.view = view; 
+
+      this.addKeyListener(this);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent a)
+    {
+      switch(a.getKeyChar())
+      {
+        case 'L':
+        case 'l':
+
+          break;
+        case 'R':
+        case 'r':
+          break;
+        case 'U':
+        case 'u':
+          break;
+        case 'D':
+        case 'd':
+          break; 
+        default:
+          System.out.println("Invalid input!"); 
+      }
+      model.updateTurn();
+      model.checkWin();
+      
+    }
+
+    @Override
+    public void keyPressed(KeyEvent a)
+    {
+      //intentionally empty, must be present
+    }
+
+    @Override
+    public void keyReleased(KeyEvent a)
+    {
+      //intentionally empty, must be present
+    }
+
 }
