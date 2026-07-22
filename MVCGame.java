1546d0dda1d6b69d0d86538ee2446ebe5cadfaa0
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-07-22 15:08:23 +0800

    Add files via upload

---
+++ MVCGame.java
@@ -0,0 +1,9 @@
+public class MVCGame
+{
+    public static void main(String[] args)
+    {
+        GameView view = new GameView(); 
+        Game model = new Game(); 
+        GameController = new GameController();
+    }
+}
