d12a05c7c9124dabda401e8c1440e6b11f37a5c8
akinicoledev <aki_cartago@dlsu.edu.ph>
2026-07-24 23:12:24 +0800

    Update GameController.java

--- controller/GameController.java
+++ controller/GameController.java
@@ -1,8 +1,10 @@
 package controller;
 
 import java.awt.event.*;
+import javax.swing.JButton;
 import model.Game;
 import model.Animal;
+import model.Space;
 import view.GameView;
 
 /**
@@ -10,7 +12,7 @@ import view.GameView;
  * it receives the player's keyboard input & updates game accordingly.
  */
 
-public class GameController implements KeyListener
+public class GameController implements KeyListener, ActionListener
 {
     /* stores the game model */
     private final Game model;
@@ -34,7 +36,63 @@ public class GameController implements KeyListener
 
         this.view.addKeyListener(this);
         this.view.setFocusable(true);
-        this.view.requestFocusInWindow();
+
+        JButton[][] buttons = this.view.getBoardButtons();
+        for (int r = 0; r < 9; r++)
+        {
+            for (int c = 0; c < 7; c++)
+            {
+                buttons[r][c].addActionListener(this);
+            }
+        }
+    }
+
+    /**
+     * handles piece selection when a board button is clicked.
+     *
+     * @param e the action event from the board button
+     */
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        JButton[][] buttons = view.getBoardButtons();
+
+        for (int r = 0; r < 9; r++)
+        {
+            for (int c = 0; c < 7; c++)
+            {
+                if (e.getSource() == buttons[r][c])
+                {
+                    Space space = model.getBoard().getSpace(r, c);
+                    if (space != null)
+                    {
+                        Animal clickedAnimal = space.getAnimal();
+
+                        if (clickedAnimal != null)
+                        {
+                            if (model.getCurrentTurn() != -1 && clickedAnimal.getOwnerId() != model.getCurrentTurn())
+                            {
+                                view.updateStatus("That's not your animal!");
+                                selectedAnimal = null;
+                            }
+                            else
+                            {
+                                selectedAnimal = clickedAnimal;
+                                view.updateStatus("Selected: " + selectedAnimal.getName() + ". Use U/D/L/R to move.");
+                            }
+                        }
+                        else
+                        {
+                            view.updateStatus("No animal on this space.");
+                            selectedAnimal = null;
+                        }
+                    }
+
+                    view.requestFocusInWindow();
+                    return;
+                }
+            }
+        }
     }
 
     /**
@@ -75,23 +133,30 @@ public class GameController implements KeyListener
                 break;
 
             default:
-                view.showMessage("Invalid input!");
+                view.updateStatus("Invalid input! Use U, D, L, or R.");
                 return;
         }
 
         model.updateTurn();
         model.checkWin();
 
-        view.updateStatus("Player " + model.getCurrentTurn() + "'s turn");
-        view.refreshBoard();
+        if (model.getWinner() != null)
+        {
+            view.showMessage("Player " + model.getWinner().getPlayerNum() + " wins!");
+        }
+        else
+        {
+            view.updateStatus("Player " + model.getCurrentTurn() + "'s turn");
+        }
 
+        view.refreshBoard(model);
+        view.requestFocusInWindow();
         selectedAnimal = null;
     }
 
     /**
      * required by the KeyListener interface.
      */
-    
     @Override
     public void keyPressed(KeyEvent a)
     {
@@ -101,7 +166,6 @@ public class GameController implements KeyListener
     /**
      * required by the KeyListener interface.
      */
-    
     @Override
     public void keyReleased(KeyEvent a)
     {
