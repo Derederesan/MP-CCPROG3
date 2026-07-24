aa52585ffecafc62bff25cebba85e9ce550752ae
akinicoledev <aki_cartago@dlsu.edu.ph>
2026-07-24 20:46:06 +0800

    connected view and controller

--- MVCGame.java
+++ MVCGame.java
@@ -1,13 +1,23 @@
 import model.Game;
-import view.Game;
-import controller.GameController
+import model.Player;
+import view.GameView;
+import controller.GameController;
 
 public class MVCGame
 {
     public static void main(String[] args)
     {
+        /* initializes players */
+        Player player1 = new Player(1);
+        Player player2 = new Player(2);
+
+        /* initializes game model */
+        Game model = new Game(player1, player2);
+
+        /* initialize the game view */
         GameView view = new GameView(); 
-        Game model = new Game(); 
+
+        /* connects the model and view through the controller */
         GameController controller = new GameController(model,view);
     }
 }
