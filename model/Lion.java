e36f257ca27f66563ddb1f6d4f4495ceaa60f460
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-07-30 16:24:06 +0800

    Update Lion.java

--- model/Lion.java
+++ model/Lion.java
@@ -13,4 +13,13 @@ public class Lion extends BigCat
     {
         super("Lion", 7, position, 0, 0, 0);
     }
+     /**
+    *  Constructs a new Space that takes the input provided 
+    * and assigns it to the specified attributes of the class.
+    */
+    public Lion(String name, int rank, Space position, int ownerId, int col, int row)
+    {
+       super(name, rank, position, ownerId, col, row);
+    }
+
 }
