13ff80f37e9d3d30a173c57b7ca1f590e8bef523
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-07-31 05:55:48 +0800

    Update Tiger.java

--- model/Tiger.java
+++ model/Tiger.java
@@ -6,15 +6,6 @@ package model;
     
 public class Tiger extends BigCat
 {
-    /**
-    * Constructs the a new Tiger that takes the provided input and assigns it 
-    * to the specified attributes of the class 
-    */
-
-    public Tiger(Space position)
-    {
-        super("Tiger", 6, position, 0, 0, 0);
-    }
 
     /**
     * Constructs the a new Tiger that takes the provided input and assigns it 
