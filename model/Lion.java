00ff254c8d06df7d0c1b68bc21555b7025e76786
akinicoledev <aki_cartago@dlsu.edu.ph>
2026-08-02 02:14:28 +0800

    Update Lion.java

--- model/Lion.java
+++ model/Lion.java
@@ -1,17 +1,16 @@
 package model;
 
 /**
-* A lion is the subclass of the abstract superclass BigCat 
-*/
-public class Lion extends BigCat
+ * A lion is the subclass of the abstract superclass BigCat
+ */
+public class Lion extends BigCat 
 {
-     /**
-    *  Constructs a new Space that takes the input provided 
-    * and assigns it to the specified attributes of the class.
-    */
-    public Lion(String name, int rank, Space position, int ownerId, int col, int row)
+    /**
+     * Constructs a new Space that takes the input provided
+     * and assigns it to the specified attributes of the class.
+     */
+    public Lion(Space position, int ownerId, int col, int row)
     {
-        super(name, rank, position, ownerId, col, row);
+        super("Lion", 7, position, ownerId, col, row);
     }
-
 }
