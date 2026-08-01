51a0015b6749ae5a01b3d8a700e27d8dfcbbcaab
akinicoledev <aki_cartago@dlsu.edu.ph>
2026-08-02 02:15:11 +0800

    Update River.java

--- model/River.java
+++ model/River.java
@@ -1,15 +1,15 @@
 package model;
 /**
-* A River is a child class that inherits the methods and attributes of Space. 
-*/
+ * A River is a child class that inherits the methods and attributes of Space.
+ */
 public class River extends Space{
 
-     /**
-    *  Constructs a new River that takes the input provided 
-    * and assigns it to the specified attributes of the class.
-    */
+    /**
+     *  Constructs a new River that takes the input provided
+     * and assigns it to the specified attributes of the class.
+     */
     public River(int ownerId)
     {
-        super("River", null, ownerId); 
+        super("River", ownerId);
     }
 }
