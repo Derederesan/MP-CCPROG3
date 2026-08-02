75ff7158f99c3c8190b078268da4e2fe9faf10e7
akinicoledev <aki_cartago@dlsu.edu.ph>
2026-08-03 04:32:07 +0800

    Update Game.java

--- model/Game.java
+++ model/Game.java
@@ -1,17 +1,155 @@
 package model;
 
-
 /**
- * A Land is a child class that inherits the methods and attributes of Space.
+ * A game allows the initialization of players and board. It keeps track of the current turn,
+ * serves as the checker on which players should go first, and checks if a player has won.
  */
-public class Land extends Space
-{
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Game {
+    private int currentTurn = -1;
+    private Player player1;
+    private Player player2;
+    private Board board;
+    private boolean isGameOver;
+    private Player winner;
+    private int moveCount = 0;
+
     /**
-     *  Constructs a new Land that takes the input provided
-     * and assigns it to the specified attributes of the class.
+     * Constructs a new Game that takes the input provided and assigns it
+     * to the specified attributes of the class.
      */
-    public Land(int ownerId)
-    {
-        super("Land", ownerId);
+    public Game(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = new Board();
+        this.isGameOver = false;
+        this.winner = null;
     }
+
+    /**
+     * Used to shuffle the ranks of the animals
+     *
+     * @return the shuffled ranks of the animals
+     */
+
+    public ArrayList<Integer> shuffleRanks()
+    {
+        ArrayList<Integer> ranks = new ArrayList<>();
+
+        for (int i = 1; i <= 8; i++)
+        {
+            ranks.add(i);
+        }
+
+        Collections.shuffle(ranks);
+
+        return ranks;
+    }
+
+    /**
+     * Compares the chosen animal piece of each player
+     * and uses it to determine which player goes first.
+     *
+     * @param p1Choice the chosen animal piece of player 1
+     * @param p2Choice the chosen animal piece of player 2
+     * @return the player that goes first
+     */
+    public int firstPick(Animal p1Choice, Animal p2Choice) {
+        if (p1Choice.getRank() > p2Choice.getRank()) {
+            //System.out.println("Player 1 moves first.");
+            this.currentTurn = 1;
+            return 1;
+        } else if (p1Choice.getRank() < p2Choice.getRank()) {
+            //System.out.println("Player 2 moves first.");
+            this.currentTurn = 2;
+            return 2;
+        } else {
+            //System.out.println("Tie! Players must pick again.");
+            return -1;
+        }
+    }
+
+    /**
+     * Keeps track of which player is currently playing
+     */
+    public void updateTurn() {
+        if (this.currentTurn == 1) {
+            this.currentTurn = 2;
+        } else if (this.currentTurn == 2) {
+            this.currentTurn = 1;
+        }
+    }
+
+    /*
+     *checks if a player has won
+     */
+    public void checkWin()
+    {
+        Player currentPlayer = (this.currentTurn == 1) ? this.player1 : this.player2;
+
+
+        ArrayList<Animal> animals= currentPlayer.getAnimals();
+
+        for(Animal animal : animals)
+        {
+            Space curSpace = animal.getCurrentSpace();
+
+            if(curSpace.isEnemyDen(currentTurn))
+            {
+                this.winner = currentPlayer;
+                this.isGameOver = true;
+            }
+        }
+        for (int r = 0; r < Board.ROWS; r++)
+        {
+            for (int c = 0; c < Board.COLS; c++)
+            {
+                Space space = this.board.getSpace(r, c);
+                if (space != null && space.getAnimal() != null)
+                {
+                    Animal animal = space.getAnimal();
+                    if (animal.getOwnerId() == this.currentTurn && space.isEnemyDen(this.currentTurn))
+                    {
+                        this.winner = currentPlayer;
+                        this.isGameOver = true;
+                        System.out.println("Player " + this.currentTurn + " has won!");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns the current player
+     *
+     * @return the current turn of the player
+     */
+    public int getCurrentTurn() {
+        return this.currentTurn;
+    }
+
+    /**
+     * Returns the winner
+     *
+     * @return the winner
+     */
+    public Player getWinner() {
+        return this.winner;
+    }
+
+    public Board getBoard(){
+        return this.board;
+    }
+    public void incrementMoveCount() {
+        moveCount++;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+
 }
