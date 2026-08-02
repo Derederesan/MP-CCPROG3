21ed75ae3373fd3c5ccbe2c585a069cb88682db1
akinicoledev <aki_cartago@dlsu.edu.ph>
2026-08-03 04:32:20 +0800

    Update Land.java

--- model/Land.java
+++ model/Land.java
@@ -4,7 +4,8 @@ package model;
 /**
  * A Land is a child class that inherits the methods and attributes of Space.
  */
-public class Land extends Space{
+public class Land extends Space
+{
     /**
      *  Constructs a new Land that takes the input provided
      * and assigns it to the specified attributes of the class.
